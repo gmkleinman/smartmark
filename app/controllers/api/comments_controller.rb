35deef4ac8f41aca8624036a1fcb2b2c8fc81d7d
gmkleinman <gmkleinman@gmail.com>
2020-06-09 23:07:55 -0700

    Additional backend comment work.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -8,5 +8,35 @@ class Api::CommentsController < ApplicationController
         end
     end
 
-    
+    def create
+        @comment = Comment.new(comment_params)     
+        if @comment.save
+            render :show
+        else
+            render json: @comment.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @comment = Comment.find_by(id: params[:id])
+        if @comment
+            @comment.update(comment_params)
+            render :show    
+        else
+            render json: ['Comment not found'], status: 404
+        end
+    end
+
+    def destroy
+        @comment = Comment.find_by(id: params[:id])
+        if @comment
+            @comment.destroy
+        end
+        render json: ['This is a placeholder - what to render when deleted?']
+    end
+
+    protected
+    def comment_params
+        params.require(:comment).permit(:body, :passage_id, :commenter_id)
+    end
 end
